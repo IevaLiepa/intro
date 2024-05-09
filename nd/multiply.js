34d9727f83caf917cb7975eecd053d0608ff8ed1
IevaLiepa <167983954+IevaLiepa@users.noreply.github.com>
2024-05-09 13:16:40 +0300

    multiply

---
+++ nd/multiply.js
@@ -0,0 +1,16 @@
+/*
+-jeigu, pirmas parametras nera skaicius
+  -graziname klaidos pranesima
+-jeigu, antras prametras nera skaicius
+  -graziname klaidos pranesima
+
+-atskirame kintamajame issisaugome sandaugos reiksme
+-graziname sandaugos reiksme
+*/
+
+//2
+function multiply(firstNumber, secondNumber) {
+    const answer = firstNumber * secondNumber;
+    return answer;
+}
+console.log(multiply(2, 2));
