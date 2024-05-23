03e65441a0ab5ff116fde3e6b34a16f699d30d58
IevaLiepa <167983954+IevaLiepa@users.noreply.github.com>
2024-05-23 13:59:54 +0300

    array.m

---
+++ metodai/array.m.js
@@ -0,0 +1,9 @@
+const desimtZuikiu = [];
+for (let i = 0; i < 10; i++) {
+    desimtZuikiu.push('zuikis');
+}
+console.log(desimtZuikiu);
+//-----
+
+console.log([1, 2, 3].flat());
+console.log([1, 2, 3, [4, 5]].flat());
